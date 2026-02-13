bd8b5ec4fa4a070572416347414c69ada409d8da
ayat-gamal <ayat.gamal@fawry.com>
2026-02-13 21:33:13 +0200

    craet main app screen files

---
+++ app/src/main/java/com/example/pokemon/presentation/homeScreen/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.pokemon.presentation.homeScreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text("HomeScreen")
+    }
+}
---
+++ app/src/main/java/com/example/pokemon/presentation/searchScreen/SearchScreen.kt
@@ -0,0 +1,16 @@
+package com.example.pokemon.presentation.searchScreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Search Screen")
+    }
+}
